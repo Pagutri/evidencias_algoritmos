ebbd713e4c0fee0911a7efbaa8f4f3197a8ba604
Pagutri <pagutri@gmail.com>
2018-09-06 14:20:02 -0500

    Avance del adp_merge

--- JNTUV4_codechef/src/adp_merge.c
+++ JNTUV4_codechef/src/adp_merge.c
@@ -11,29 +11,29 @@
  */
 
 #include "libraries.h"
-  
-#define VERBOSE_N 1000
-
 
 int main()
 {
 unsigned int n;
+unsigned int resp = 0;
 int *S;
 int x;
 
 scanf("%i", &n);
-allocate_uintarray(&S, n);
+allocate_array(&S, n);
 save_array(S, n);
 scanf("%i", &x);
+
+resp = merge_sort(S, n);
 
 return(1);
 }
 
 
 
-unsigned int allocate_uintarray(unsigned int **array, unsigned int N)
+unsigned int allocate_array(int **array, unsigned int N)
 {
-*array = (unsigned int *)malloc(N * sizeof(unsigned int));
+*array = (int *)malloc(N * sizeof(int));
 if(NULL == *array)
   {
   printf("\n\nMemory allocation error in function\n"\
@@ -57,7 +57,7 @@ for(i = 0; i < arraylen; i++)
 
 
 
-int copy_uintarray(unsigned int *src, unsigned int *dst, unsigned int N)
+int copy_array(int *src, int *dst, unsigned int N)
 {
 unsigned int i;
 
@@ -71,52 +71,51 @@ return(i+1);
 
 
 
-unsigned int merge_sort(unsigned int *array, unsigned int N)
+unsigned int merge_sort(int *array, unsigned int N)
 {
-unsigned int *buffer;
-unsigned int operations = 0;
+int *buffer;
+unsigned int resp = 0;
 
-allocate_uintarray(&buffer, N);
+allocate_array(&buffer, N);
 
-copy_uintarray(array, buffer, N);
+copy_array(array, buffer, N);
 
-operations = N + split_merge_sorted(buffer, array, 0, N-1);
+resp = split_merge_sorted(buffer, array, 0, N-1, 0);
 
 free(buffer);
-return(operations);
+return(resp);
 }
 
 
 
-unsigned int split_merge_sorted(unsigned int *src, unsigned int *dst,\
-                                unsigned int firstindex, unsigned int lastindex)
+unsigned int split_merge_sorted(int *src, int *dst,\
+                                unsigned int firstindex, unsigned int lastindex\
+                                unsigned int resp)
 {
 unsigned int middleindex;
-unsigned int returnvalue = 0;
 
 if(lastindex - firstindex < 1)
   {
-  return(0);
+  return(resp);
   }
 
 middleindex = (firstindex + lastindex) / 2;
 
-returnvalue += split_merge_sorted(dst, src, firstindex, middleindex);
-returnvalue += split_merge_sorted(dst, src, middleindex+1, lastindex);
+resp = split_merge_sorted(dst, src, firstindex, middleindex, resp);
+resp = split_merge_sorted(dst, src, middleindex+1, lastindex, resp);
 
-returnvalue += merge_sorted(src, dst, firstindex, middleindex, lastindex);
+resp = merge_sorted(src, dst, firstindex, middleindex, lastindex, resp);
 
-return(returnvalue);
+return(resp);
 }
 
 
 
-unsigned int merge_sorted(unsigned int *src, unsigned int *dst,\
+unsigned int merge_sorted(int *src, int *dst,\
                           unsigned int firstindex,\
                           unsigned int middleindex,\
-                          unsigned int lastindex)
+                          unsigned int lastindex, unsigned int resp)
 {
-unsigned int sorted_numbers = 0;
 unsigned int lo_srcindex, hi_srcindex, dstindex;
 
 lo_srcindex = firstindex;
@@ -126,7 +125,7 @@ for(dstindex = firstindex; dstindex <= lastindex; dstindex++)
   {
   if(lo_srcindex <= middleindex) /* Numbers remain in the lower array part */
     {
-    if(hi_srcindex > lastindex) /* No numbers reamin in the higher array part */
+    if(hi_srcindex > lastindex) /* No numbers remain in the higher array part */
       {
       dst[dstindex] = src[lo_srcindex];
       lo_srcindex++;
@@ -150,9 +149,8 @@ for(dstindex = firstindex; dstindex <= lastindex; dstindex++)
     dst[dstindex] = src[hi_srcindex];
     hi_srcindex++;
     }
-  sorted_numbers++;
   }
 
-return(sorted_numbers);
+return(resp);
 }
 
--- JNTUV4_codechef/src/adp_merge.h
+++ JNTUV4_codechef/src/adp_merge.h
@@ -2,23 +2,22 @@
 #define __3k9_MERGESORT_H
 
 
-unsigned int allocate_uintarray(unsigned int **array, unsigned int N);
+unsigned int allocate_array(int **array, unsigned int N);
 
-unsigned int randomize_uintarray(unsigned int *array,\
-                                 unsigned int arraylen,\
-                                 unsigned int randmax);
+void save_array(int *array, unsigned int arraylen);
 				 
-int copy_uintarray(unsigned int *src, unsigned int *dst, unsigned int N);
+int copy_array(int *src, int *dst, unsigned int N);
 
-unsigned int merge_sort(unsigned int *array, unsigned int N);
+unsigned int merge_sort(int *array, unsigned int N);
 
-unsigned int split_merge_sorted(unsigned int *src, unsigned int *dst,\
-                                unsigned int firstindex, unsigned int lastindex);
+unsigned int split_merge_sorted(int *src, int *dst,\
+                                unsigned int firstindex, unsigned int lastindex\
+                                unsigned int resp);
 
-unsigned int merge_sorted(unsigned int *src, unsigned int *dst,\
+unsigned int merge_sorted(int *src, int *dst,\
                           unsigned int firstindex,\
                           unsigned int middleindex,\
-                          unsigned int lastindex);
+                          unsigned int lastindex, unsigned int resp);
 
 
 #endif
