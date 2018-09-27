26afae59e14e8e9719cdcd76216cde5c3d08df1c
Gutierrez Rivera Salma Patricia <gutierrezrs2015@dci25-187.ugto.mx>
2018-09-27 10:00:35 -0500

    mas intentos con alphacode

--- alphacode_sol.py
+++ alphacode_sol.py
@@ -1,41 +1,44 @@
 import fileinput
+from numpy import zeros
 
-# Esta es la funcion que encuentra el numero de decodificaciones posibles
-# Asume que encryption es tipo string
+# Asume que encryption es tipo string y que no hay encriptaciones no permitidas
+
+def memoized_N(i, chain, N):
+    if i == 0:
+        return 1
+    elif i == 1 and (int(chain[0] + chain[1]) > 26 or \
+        int(chain[1]) == 0):
+        N[i] = 1
+        return N[i]
+    elif i == 1:
+        N[i] = 2
+        return N[i]
+    else:
+        if int(chain[i - 1] + chain[i]) > 26:
+            if N[i - 1] == 0:
+                N[i - 1] = memoized_N(i - 1, chain, N)
+            return N[i - 1]
+        elif int(chain[i]) == 0:
+            if N[i - 2] == 0:
+                N[i - 2] = memoized_N(i - 2, chain, N)
+            return N[i - 2]
+        else:
+            if N[i - 1] == 0:
+                N[i - 1] = memoized_N(i - 1, chain, N)
+            if N[i - 2] == 0:
+                N[i - 2] = memoized_N(i - 2, chain, N)
+            return N[i - 1] + N[i - 2]
 
 def num_decod(encryption):
     m = len(encryption)
-    encryption = encryption[:m - 1] # Quitar el salto de linea
-    m -= 1
-    N = []
+    N = zeros(m, dtype = int)
+    N[0] = 1
     
-    if m == 1 or (m == 2 and (int(encryption[0] + encryption[1]) > 26 or \
-        int(encryption[1]) == 0)):
-        return 1
-    elif m == 2:
-        return 2
-    else:
-        N.append(1)
-        
-        if int(encryption[0] + encryption[1]) > 26 or int(encryption[1]) == 0:
-            N.append(1)
-        else:
-            N.append(2)
-        
-        for i in range(2, m):
-            if int(encryption[i - 1] + encryption[i]) > 26 or \
-            int(encryption[i]) == 0 or int(encryption[i - 1]) == 0:
-                N.append(N[i - 1])
-            elif int(encryption[i - 1] + encryption[i]) <= 26 and \
-            i + 1 < m and int(encryption[i + 1]) == 0:
-                N.append(N[i - 1])
-            else:
-                N.append(N[i - 1] + N[i - 2])
-    
-    return N[m - 1]
+    return memoized_N(m - 1, encryption, N)
 
 # Aqui empieza el programa
 
 for line in fileinput.input():
+    line = line[:len(line) - 1] # Quitar salto de linea
     if line != '0':
         print(num_decod(line))
